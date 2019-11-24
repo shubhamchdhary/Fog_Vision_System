064ff1ac8e566f5e92e02b94638170b5379d13ea
shubhmachaudhary <shubhamchaudharybrg@gmail.com>
2019-11-24 21:03:35 +0530

    Python file to control the two servos.

--- servoControl.py
+++ servoControl.py
@@ -1,7 +1,29 @@
 import servo as sr
 
-servo1 = sr.Servo(18)
-servo1.start(50)
-servo1.run(0,180,0.05)
+ser1 = sr.Servo(16)
+ser1.start(50)
+ser2 = sr.Servo(18)
+ser2.start(50)
 
+step = 0
+flag = 0 # Flag to check up or down motion. 0 for downward and 1 for upward.
 
+while True:
+	ser1.slide(0,240,0.05)
+	if flag == 0:
+		step += 15
+	elif flag == 1:
+		step -= 15
+	ser2.goto(step)
+
+	ser1.slide(240,0,0.05)
+	if flag == 0:
+		step += 15
+	elif flag == 1:
+		step -= 15
+	ser2.goto(step)
+
+	if step == 180:
+		flag = 1
+	elif step == 0:
+		flag = 0
