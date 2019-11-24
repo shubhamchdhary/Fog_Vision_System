9fc8ebffbb405c8dadef863a448f594de9b8d8f1
shubhmachaudhary <shubhamchaudharybrg@gmail.com>
2019-11-24 19:30:01 +0530

    Omits while loop.

--- servo.py
+++ servo.py
@@ -24,22 +24,21 @@ class Servo():
 		self.pwm.ChangeDutyCycle(self.duty)
 
 # Continuously runs servo between two angles.
-	def run(self,startAngle,endAngle,stepDelay): #Angles in multiples of 15 degrees
+	def sweep(self,startAngle,endAngle,stepDelay): #Angles in multiples of 15 degrees
 		self.sAngle = startAngle/15
 		self.eAngle = endAngle/15
 		self.delay = stepDelay
-		while True:
-			for i in range(self.sAngle,self.eAngle):
-				self.pwm.ChangeDutyCycle(i)
-				time.sleep(self.delay)
-			for j in range(self.eAngle,self.sAngle,-1):
-				self.pwm.ChangeDutyCycle(j)
-				time.sleep(self.delay)
+		for i in range(self.sAngle,self.eAngle):
+			self.pwm.ChangeDutyCycle(i)
+			time.sleep(self.delay)
+		for j in range(self.eAngle,self.sAngle,-1):
+			self.pwm.ChangeDutyCycle(j)
+			time.sleep(self.delay)
 
 
 
 if __name__ == "__main__":
 	servo1 = Servo(18)
 	servo1.start(60)
-	servo1.run(60,120,0.05)
+	servo1.sweep(60,120,0.05)
 
