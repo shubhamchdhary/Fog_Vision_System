ff6b18d8b0f77e42163a6d034d12063147d9f14f
shubhmachaudhary <shubhamchaudharybrg@gmail.com>
2019-12-19 15:26:03 +0530

    Update angle conversion.

--- servo.py
+++ servo.py
@@ -25,24 +25,27 @@ class Servo():
 
 # Continuously runs servo between two angles.
 	def sweep(self,startAngle,endAngle,stepDelay): #Angle in degree from 2 to 180.
-		self.sStep = (startAngle*0.055)+2
-		self.eStep = (endAngle*0.055)+2
+		self.sAngle = startAngle
+		self.eAngle = endAngle
 		self.delay = stepDelay
-		for i in range(self.sStep,self.eStep):
-			self.pwm.ChangeDutyCycle(i)
+		for i in range(self.sAngle,self.eAngle):
+			step = (i*0.055)+2
+			self.pwm.ChangeDutyCycle(step)
 			time.sleep(self.delay)
-		for j in range(self.eStep,self.sStep,-1):
-			self.pwm.ChangeDutyCycle(j)
+		for j in range(self.eAngle,self.sAngle,-1):
+			step = (j*0.055)+2
+			self.pwm.ChangeDutyCycle(step)
 			time.sleep(self.delay)
 
 
 # Moves a servo once between defined positions.
 	def slide(self,startAngle,endAngle,stepDelay):
-		self.sStep = (startAngle*0.055)+2
-		self.eAngle = (endAngle*0.055)+2
+		self.sAngle = startAngle
+		self.eAngle = endAngle
 		self.delay = stepDelay
 		for ii in range(self.sStep,self.eStep):
-			self.pwm.ChangeDutyCycle(ii)
+			step = (ii*0.055)+2
+			self.pwm.ChangeDutyCycle(step)
 			time.sleep(self.delay)
 
 
